619c524ea03512abcb8f4dc82a3542632934eeec
Đăng Minh <112609817+mintcd@users.noreply.github.com>
2023-06-05 16:30:23 +0700

    Modified non-centered elements

--- src/views/intro.jsx
+++ src/views/intro.jsx
@@ -1,11 +1,14 @@
 const Intro = () => {
     return (
         <div class="intro-container">
-            <h1> Why is there? </h1>
-            <ul class="intro-list">
-                <li class="intro-element"> SCRUM - a fragile development framework containing worth-acquiring values </li>
-                <li class="intro-element"> New to SCRUM? Think of this as a co-working ability test!</li>
-            </ul>
+            <h1 class="intro-titile"> Why is there? </h1>
+            <div class="intro-list">
+                <ul >
+                    <li class="intro-element"> SCRUM - a fragile development framework containing worth-acquiring values </li>
+                    <li class="intro-element"> New to SCRUM? Think of this as a co-working ability test!</li>
+                </ul>
+            </div>
+
         </div>
     )
 }
